4e44a9dcc6a84267e92d17dae1e87b12c7d77f1a
Mubashar Nisar <mubasharnisar32@gmail.com>
2022-03-13 23:31:56 +0500

    frontend completed

--- app/run_dl_server.py
+++ app/run_dl_server.py
@@ -17,52 +17,26 @@ app = flask.Flask(__name__)
 model = None
 
 
-
-@app.route("/", methods=["GET","POST"])
-
+@app.route("/", methods=["GET", "POST"])
 def upload_predict():
-  if request.method == "POST":
-    image_file = request.files["image"]
-
-		# image = pil_img.open(io.BytesIO(image_file))
-		# image = T.ToTensor()(image)
-		# image = Image(image)
-    if image:
-      # CLI = cli.Prediction(image_file,paper_size).pred_result()
-      result = load_model(image_file)
-      pred = 'Predection = '+str(result)
-      return render_template("index.html",pred='{}'.format(pred) )    
-  return render_template("index.html",prediction = 0)
+    if request.method == "POST":
+        image_file = request.files["image"].read()
+        image = pil_img.open(io.BytesIO(image_file))
+        image = T.ToTensor()(image)
+        image = Image(image)
+        if image:
+            # CLI = cli.Prediction(image_file,paper_size).pred_result()
+            result = load_model(image)
+            pred = 'Predection: '+str(result)
+            return render_template("index.html", pred='{}'.format(pred))
+    return render_template("index.html", prediction=0)
 
 
 def load_model(image):
-	global model
-	model = load_learner('' , 'Model_densenet121_new.pkl')
-	return model.predict(image)[0]
+    global model
+    model = load_learner('', 'Model_densenet121_new.pkl')
+    return model.predict(image)[0]
+
 
 if __name__ == "__main__":
-  app.run(port = 5000, debug = True)
-# @app.route("/predict", methods=["POST"])
-# def predict():
-# 	# ensure an image was properly uploaded to our endpoint
-# 	if flask.request.method == "POST":
-# 		if flask.request.files.get("image"):
-			# read the image in PIL format
-			# image = flask.request.files["image"].read()
-			# image = pil_img.open(io.BytesIO(image))
-			# image = T.ToTensor()(image)
-			# image = Image(image)
-# 			data["prediction"] = []
-# 			data["prediction"].append(str(model.predict(image)[0]))
-# 			data["success"] = True
-
-# 	# return the data dictionary as a JSON response
-# 	return flask.jsonify(data)
-
-# if this is the main thread of execution first load the model and
-# then start the server
-# if __name__ == "__main__":
-# 	print(("* Flask starting server..."
-# 		"please wait until server has fully started"))
-# 	load_model()
-# 	app.run()
+    app.run(port=5000, debug=True)
